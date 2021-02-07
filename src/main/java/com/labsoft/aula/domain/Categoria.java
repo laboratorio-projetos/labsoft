ceaec2040976b96da210a16ccb8b9a53a7194eba
Daniel Saraiva <daniel.saraiva@prof.ce.gov.br>
2021-02-06 22:43:43 -0300

    Criando repository e service para categoria

--- src/main/java/com/labsoft/aula/domain/Categoria.java
+++ src/main/java/com/labsoft/aula/domain/Categoria.java
@@ -14,7 +14,7 @@ public class Categoria implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String nome;
+	private String nome; 
 	
 	public Categoria() {
 		
